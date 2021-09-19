72da68a1d7599ce0492213d0a8e820c8e7ec362e
Jessica Ortner <jortner1222@gmail.com>
2021-09-18 18:59:15 -0700

    attempting to fix broken link

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const links =[
    
     {
         label: "Week 1 Notes",
-        url: "/Portfolio/Week1/Week 1 notes.html" 
+        url: "https://github.com/jortner1222/Portfolio/blob/main/Week1/Week%201%20notes.html" 
     }
 ]
 let list = document.getElementById("TableOfContents")

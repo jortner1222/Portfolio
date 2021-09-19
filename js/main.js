d7c44441d53521bb1168758a1b4b2d2655089e6c
Jessica Ortner <jortner1222@gmail.com>
2021-09-18 17:32:59 -0700

    made the array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-
+const links =[
+    {
+        label: "Index",
+        url: "../index.html"
+    },
+   
+    {
+        label: "Week 1 Notes",
+        url: "../Week1/Week 1 notes.html" 
+    }
+]

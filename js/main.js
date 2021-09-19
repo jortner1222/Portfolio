d039f8752f1757219372223b64bd2084f82845da
Jessica Ortner <jortner1222@gmail.com>
2021-09-18 18:48:50 -0700

    fixed bug

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let list = document.getElementById("TableOfContents")
+
 const links =[
     {
         label: "Index",
@@ -10,7 +10,8 @@ const links =[
         url: "../Week1/Week 1 notes.html" 
     }
 ]
-
+let list = document.getElementById("TableOfContents")
+function loadList(){
 links.forEach((item)=> {
 let li = document.createElement("li")
 let a =document.createElement("a")
@@ -18,4 +19,4 @@ a.textContent= item.label
 a.setAttribute('href', item.url)
 li.appendChild(a);
 list.appendChild(li)
-})
+})}

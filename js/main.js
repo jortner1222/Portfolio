773622d8a0a63bb3ba8a6c10477551442a75553f
Jessica Ortner <jortner1222@gmail.com>
2021-10-30 16:01:29 -0700

    fixing file name issues

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ const links =[
     },
     {
         label: "Week 7 Notes",
-        url: "../Portfolio/Week7/week7 notes.html"
+        url: "../Portfolio/Week7/Week 7 notes.html"
     },
 ]
 let list = document.getElementById("TableOfContents")
